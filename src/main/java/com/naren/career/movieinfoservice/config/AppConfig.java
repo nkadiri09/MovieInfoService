21eea85bb30d4a18d2ce83ad95b42efbbb88bd49
“nkadiri09@gmail.com” <nkadiri09@gmail.com>
2024-03-18 15:23:06 -0400

    Updated config

--- src/main/java/com/naren/career/movieinfoservice/config/AppConfig.java
+++ src/main/java/com/naren/career/movieinfoservice/config/AppConfig.java
@@ -1,14 +1,16 @@
 package com.naren.career.movieinfoservice.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class AppConfig implements WebMvcConfigurer {
 
     @Override
-    public void addInterceptor(InterceptorRegistry registry) {
+    public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new ApiLogger()).addPathPatterns("/v1/*");
     }
 
